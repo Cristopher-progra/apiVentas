bf904f792450bb306b951ef4e7c6260b2f9b9ca1
Cristopher <cristophervalle696@gmail.com>
2023-11-04 18:24:05 -0600

    controlador y ruta api

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaisController;
 use App\Http\Controllers\ClienteController;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,12 +15,19 @@ use App\Http\Controllers\ClienteController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::get('/cliente/select', [ClienteController::class, 'select' ]);
-Route::post('/cliente/store', [ClienteController::class, 'store' ]);Route::put('/cliente/update/{id}', [ClienteController::class, 'update' ]);
-Route::delete('/cliente/delete/{id}', [ClienteController::class, 'delete' ]);
-Route::get('/cliente/find/{id}', [ClienteController::class, 'find' ]);
-// Rutas para gestionar la tabla Pais
-Route::get('/pais/select', [PaisController::class, 'select' ]);
+// Rutas protegidas
+    // Rutas para gestionar la tabla Cliente
+    Route::get('/cliente/select', [ClienteController::class, 'select' ]);
+    Route::post('/cliente/store', [ClienteController::class, 'store' ]);
+    Route::put('/cliente/update/{id}', [ClienteController::class, 'update' ]);
+    Route::delete('/cliente/delete/{id}', [ClienteController::class, 'delete' ]);
+    Route::get('/cliente/find/{id}', [ClienteController::class, 'find' ]);
+    // Rutas para gestionar la tabla País
+    Route::get('/pais/select', [PaisController::class, 'select' ]);
+
+    // Rutas para gestionar usuarios
+    Route::post('/usuario/register', [UserController::class, 'register']);
+    Route::post('/usuario/login', [UserController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
